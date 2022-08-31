ff7ed50528f0bb0f463c227b788025f75ee7197a
edisemin <edisemin@hotmail.com>
2022-08-31 09:41:13 +0100

    Adding more config and boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import {  } from './pages';
 
 const App = () => {
 
-    return < />
+    return <Routes>
+                <Route path="/" element={< />} />
+                <Route path="/:" element={< />}/>
+                <Route path="*" element={<h1>Not available!</h1>}></Route>
+            </Routes>
 }
 
 export default App;

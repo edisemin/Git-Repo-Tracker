cbe96eb07792ab1dfeb1ad501b11162235deb222
B9ary01 <bishalrai111@yahoo.com>
2022-08-31 14:39:40 +0100

    add form, header, card files

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header>
+            <h1>Github Repo Tracker App</h1>
+            <em>Track git repo now!</em>
+           </header>
+}
+
+export default Header;
